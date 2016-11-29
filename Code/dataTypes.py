e580d44e12beb81ba9e6f5c807f315f333544a7a
Zach Silver <zsilver96@gmail.com>
2016-11-28 23:16:20 -0500

    fixed some formatting

--- Code/dataTypes.py
+++ Code/dataTypes.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 
+
 # abstract base class for defining labels
 class Label:
     __metaclass__ = ABCMeta
@@ -7,34 +8,36 @@ class Label:
     @abstractmethod
     def __str__(self): pass
 
-       
+
 class ClassificationLabel(Label):
     def __init__(self, label):
         # TODO
         pass
-        
+
     def __str__(self):
         # TODO
         pass
+
 
 class FeatureVector:
     def __init__(self):
         # TODO
         pass
-        
+
     def add(self, index, value):
         # TODO
         pass
-        
+
     def get(self, index):
         # TODO
         return 0.0
-        
+
 
 class Instance:
     def __init__(self, feature_vector, label):
         self._feature_vector = feature_vector
         self._label = label
+
 
 # abstract base class for defining predictors
 class Predictor:
@@ -45,5 +48,3 @@ class Predictor:
 
     @abstractmethod
     def predict(self, instance): pass
-
-       
