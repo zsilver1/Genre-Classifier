5f995ef19d197fdc71eef7c03f726af2c067787c
Zach Silver <zsilver96@gmail.com>
2016-12-06 15:19:44 -0500

    small changes

--- Code/NN.py
+++ Code/NN.py
@@ -1,4 +1,5 @@
 from dataTypes import FeatureVector, Predictor, ClassificationLabel
+import math
 
 
 class NeuralNetwork(Predictor):
@@ -7,7 +8,16 @@ class NeuralNetwork(Predictor):
         self.genre = genre
         self.numHiddenLayers = numHiddenLayers
 
-    def logistic(self, num):
+    def logistic(self, x):
+        # we will only use this in the LAST LAYER, because we are doing
+        # binary regression
+        return 1 / (1 + math.exp(-x))
+
+    def softmax(self):
+        # we could use this for multiple classes instead of multiple networks
+        pass
+
+    def crossEntropy(self, real, predicted):
         pass
 
     def train(self, instances):
--- Code/perceptron.py
+++ Code/perceptron.py
@@ -5,6 +5,8 @@ class Perceptron:
     def __init__(self, input_data):
         self.weights = FeatureVector()
         self.input_data = input_data
+        # add bias to output
+        self.bias = 0
 
     def computeOutput(self):
-        return self.input_data.dot(self.weights)
+        return self.input_data.dot(self.weights) + self.bias
